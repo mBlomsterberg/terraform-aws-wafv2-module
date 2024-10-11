4396d11366f988e1e9fbac6d3848a5076a52a012
Mikkel Blomsterberg <98821063+mBlomsterberg@users.noreply.github.com>
2024-10-11 14:21:33 +0200

    fix: Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "arn" {
+  value = aws_wafv2_web_acl.regional.arn
+}
+
+output "id" {
+  value = aws_wafv2_web_acl.regional.id
+}
+
+output "capacity" {
+  value = aws_wafv2_web_acl.regional.capacity
+}
+
+output "tags_all" {
+  value = aws_wafv2_web_acl.regional.tags_all
+}
+
+output "application_integration_url" {
+  value = aws_wafv2_web_acl.regional.application_integration_url
+}
